9d395ca6042847be212a3a6d542dea62289700e0
Felipe Merchan <felipe-sc@hotmail.es>
2022-02-06 18:48:50 -0500

    feat: ¿qué son las migraciones? Migraciones en Sequelize ORM

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -5,18 +5,22 @@ const setUpModels = require('./../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 // Por detrás va a gestionar el pooling
 const sequelize = new Sequelize(URI, {
-  dialect: 'mysql', // Indica qué base de datos estamos utilizando
+  dialect: 'postgres', // Indica qué base de datos estamos utilizando
   logging: true, //En la consola cada vez que haga una consulta con el ORM veremos cuál sería la forma de hacer esa consulta, pero en SQL
 });
 
 setUpModels(sequelize);
 
 /* Realiza una sincronización. Es decir, Sequelize va a tomar los modelos que hayamos configurado
-y va a crear la estructura de la tabla con el schema que hayamos creado */
+y va a crear la estructura de la tabla con el schema que hayamos creado.
+Esto es muy delicado y no se aconseja para producción porque necesitamos un sistema de migraciones
+que nos indique en qué punto estaba para saber qué hay que modificar, cada vez que lancemos un release
+no debemos estar creando tablas.
+*/
 sequelize.sync();
 
 module.exports = sequelize;
